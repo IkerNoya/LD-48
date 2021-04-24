d519af84ec2fc42a3879aec85c7b4a6af4d72946
FacundoPonce <mrfacu125@gmail.com>
2021-04-24 16:24:19 -0300

    Options menu working.
    Waiting for audio to test value changes.

---
+++ Assets/Scripts/Menu/UIOptions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIOptions : MonoBehaviour
+{
+    [SerializeField] public Slider sfxVolume;
+    [SerializeField] public Slider musicVolume;
+
+    private float sfxVolu = 0;
+    private float musicVolu = 0;
+    void Awake()
+    {
+        sfxVolume.value = DataManager.instance.GetSFXVolume();
+        musicVolume.value = DataManager.instance.GetMusicVolume();
+    }
+    void Update()
+    {
+        sfxVolu = sfxVolume.value;        
+        musicVolu = musicVolume.value;
+        DataManager.instance.SetSFXVolume(sfxVolu);
+        DataManager.instance.SetMusicVolume(musicVolu);
+        //Audio manager seteo de volumen
+    }
+}
