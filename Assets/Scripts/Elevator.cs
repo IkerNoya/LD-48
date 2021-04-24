a64f521512bb408dc72bfcdcf3d5600bb216d55e
FacundoPonce <mrfacu125@gmail.com>
2021-04-24 01:15:55 -0300

    Created an elevator prototype.

---
+++ Assets/Scripts/Elevator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Elevator : MonoBehaviour
+{
+    [SerializeField] public GameObject leftDoor;
+    [SerializeField] public GameObject rightDoor;
+
+    [SerializeField] public GameObject moveTo;
+
+    [SerializeField] public Vector3 initialPos;
+
+    [SerializeField]
+    public enum ElevatorState
+    {
+        Hide,
+        OnTransition,
+        InScene
+    }
+    public ElevatorState state;
+
+    private void Start()
+    {
+        initialPos = transform.position;
+    }
+    void Update()
+    {
+        
+    }
+
+    public void MoveElevator()
+    {
+
+    }
+}
