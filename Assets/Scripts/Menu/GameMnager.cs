07436f7001d635c4363ab1f9340bd72d60a6adea
FacundoPonce <mrfacu125@gmail.com>
2021-04-24 18:10:14 -0300

    Done credits and start making game Manager.

---
+++ Assets/Scripts/Menu/GameMnager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMnager : MonoBehaviour
+{
+    private static GameMnager instance;
+    public static GameMnager Get()
+    {
+        return instance;
+    }
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+    void Update()
+    {
+        
+    }
+
+    public void CheckIfPause()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+
+        }
+    }
+}
