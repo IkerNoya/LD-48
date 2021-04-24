f6863257a0578f2773e1f1ae0e2ea555d1ee8d72
Iker <ikernoya4@gmail.com>
2021-04-24 00:22:48 -0300

    Created player controller

---
+++ Assets/Scripts/MouseLook.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField] float verticalSensitivity;
+    [SerializeField] float horizontalSensitivity;
+    [SerializeField] Transform body;
+
+    float xRotation = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * horizontalSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * verticalSensitivity * Time.deltaTime; 
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation,0,0);
+        if (body!=null)
+            body.Rotate(Vector3.up * mouseX);
+    }
+}
