a35e750069c6aaaa739cc361d526384c09da479e
Iker <ikernoya4@gmail.com>
2021-04-24 18:32:53 -0300

    Created shotgun prefab

--- Assets/Scripts/HudManager.cs
+++ Assets/Scripts/HudManager.cs
@@ -6,7 +6,7 @@ using UnityEngine.UI;
 public class HudManager : MonoBehaviour
 {
     //generalize to weapons later
-    [SerializeField] Bailif weapon;
+    [SerializeField] Weapon weapon;
     [SerializeField] Text ammo;
     void Start()
     {
