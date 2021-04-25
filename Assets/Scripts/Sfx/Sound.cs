1e8f242cd4f469670d10fe53a38e2aa42f17473c
FacundoPonce <mrfacu125@gmail.com>
2021-04-25 15:10:27 -0300

    Added class sound and an adudio manager.

---
+++ Assets/Scripts/Sfx/Sound.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+[System.Serializable]
+public class Sound
+{
+    public AudioClip clip;
+
+    [Range(0.0f,1.0f)]
+    public float volume;
+
+    [Range(.1f,1.0f)]
+    public float picth;
+
+    [HideInInspector]
+    public AudioSource source;
+
+    public string name;
+
+    public bool loop;
+}
