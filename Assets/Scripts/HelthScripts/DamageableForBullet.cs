6f6d4f0bc0facf66db8da32548c6445e9d81e298
TheoLeyenda <31700515+TheoLeyenda@users.noreply.github.com>
2021-04-25 00:14:33 -0300

    TiradorLSD en progreso...

---
+++ Assets/Scripts/HelthScripts/DamageableForBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageableForBullet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private HealthSystem healthSystem;
+    void OnCollisionEnter(Collision collision)
+    {
+        Bullet bullet = collision.collider.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            healthSystem.SubstractLife(bullet.GetDamage());
+            healthSystem.CheckDieEvent();
+            Destroy(bullet.gameObject);
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Bullet bullet = other.GetComponent<Bullet>();
+        if (bullet != null)
+        {
+            healthSystem.SubstractLife(bullet.GetDamage());
+            healthSystem.CheckDieEvent();
+            Destroy(bullet.gameObject);
+        }
+    }
+}
