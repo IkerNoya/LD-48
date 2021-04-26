4917241968bdb19aa813f525736cd74c2f6abe37
Iker <ikernoya4@gmail.com>
2021-04-25 22:47:58 -0300

    Added different fire modes to weapon

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -5,38 +5,49 @@ using System;
 
 public class Weapon : MonoBehaviour
 {
-    [SerializeField] AmmoType ammoType;
-    [Space]
-    [SerializeField] protected int ammo;
-    [SerializeField] protected float fireRate;
-    [SerializeField] protected float damage;
-    [Space]
-    [SerializeField] protected float verticalRecoil;
-    [SerializeField] protected float horizontalRecoil;
-    [Space]
-    [SerializeField] protected float range;
-    [Space]
-    [SerializeField] protected float spreadX = 0.25f;
-    [SerializeField] protected float spreadY = 0.03f;
-    [Space]
-    [SerializeField] protected ParticleSystem muzzleFlash;
-    [SerializeField] protected ParticleSystem hitEnvironment;
-    [SerializeField] protected ParticleSystem hitEnemy;
+    [Serializable]
+    protected class WeaponData
+    {
+        public AmmoType ammoType;
+        
+        public int ammo;
+        public float fireRate;
+        public float damage;
+
+        public float verticalRecoil;
+        public float horizontalRecoil;
+
+        public float range;
+        public float reloadSpeed;
+
+        public float spreadX = 0.25f;
+        public float spreadY = 0.03f;
+
+        public ParticleSystem muzzleFlash;
+        public ParticleSystem hitEnvironment;
+        public ParticleSystem hitEnemy;
+
+        public AudioClip fire;
+        public AudioClip reload;
+
+    }
+    [SerializeField] protected List<WeaponData> weaponData;
+
+    [SerializeField] protected AudioSource audioSource;
     [SerializeField] protected Camera cam;
     [SerializeField] protected MouseLook mouseLook;
-    [Space]
-    [SerializeField] protected AudioSource audioSource;
-    [SerializeField] protected AudioClip shotgun;
-    [SerializeField] protected AudioClip revolver;
-    [SerializeField] protected AudioClip reload;
+
 
     int pellets = 8;
     bool isReloading = false;
+    bool isChangingWeaponMode = false;
 
-    enum AmmoType
+    public enum AmmoType
     {
         shell, bullet
     };
+
+    protected int weaponIdChoice = 0;
 
     protected bool canShoot = true;
 
@@ -46,90 +57,96 @@ public class Weapon : MonoBehaviour
 
     public static event Action<Weapon, Transform> HitDamage;
 
-    void Update()
-    {
-        switch(ammoType)
-        {
-            case AmmoType.bullet:
-                if (!isReloading) audioSource.clip = revolver;
-                break;
-            case AmmoType.shell:
-                if (!isReloading) audioSource.clip = shotgun;
-                break;
-        }
-    }
-
     protected void Shoot(ref int currentAmmo)
     {
         RaycastHit hit;
         Vector3 mousePos = Input.mousePosition;
         Ray ray = cam.ScreenPointToRay(mousePos);
 
-        switch (ammoType)
+        switch (weaponData[weaponIdChoice].ammoType)
         {
             case AmmoType.bullet:
-                Debug.DrawRay(ray.origin, ray.direction * range, Color.red);
-                if (Physics.Raycast(ray, out hit, range))
+                audioSource.clip = weaponData[weaponIdChoice].fire;
+                Debug.DrawRay(ray.origin, ray.direction * weaponData[weaponIdChoice].range, Color.red);
+                if (Physics.Raycast(ray, out hit, weaponData[weaponIdChoice].range))
                 {
                     //hit target
                     if (hit.collider.CompareTag("Enemy"))
                     {
-                        Instantiate(hitEnemy, hit.point, Quaternion.LookRotation(hit.normal));
+                        Instantiate(weaponData[weaponIdChoice].hitEnemy, hit.point, Quaternion.LookRotation(hit.normal));
                         HitDamage?.Invoke(this, hit.transform);
                     }
                     else
                     {
-                        Instantiate(hitEnvironment, hit.point, Quaternion.LookRotation(hit.normal));
+                        Instantiate(weaponData[weaponIdChoice].hitEnvironment, hit.point, Quaternion.LookRotation(hit.normal));
                     }
                 }
                 break;
             case AmmoType.shell:
+                audioSource.clip = weaponData[weaponIdChoice].fire;
                 Vector3 spread = Vector3.zero;
                 Vector3 direction = ray.direction;
                 for(int i = 0; i < pellets; i++)
                 {
-                    spread = new Vector3(direction.x + UnityEngine.Random.Range(-spreadX, spreadX), direction.y + UnityEngine.Random.Range(-spreadY, spreadY), direction.z);
-                    if (Physics.Raycast(ray.origin, spread, out hit, range))
+                    spread = new Vector3(direction.x + UnityEngine.Random.Range(-weaponData[weaponIdChoice].spreadX, weaponData[weaponIdChoice].spreadX), direction.y + UnityEngine.Random.Range(-weaponData[weaponIdChoice].spreadY, weaponData[weaponIdChoice].spreadY), direction.z);
+                    if (Physics.Raycast(ray.origin, spread, out hit, weaponData[weaponIdChoice].range))
                     {
                         //hit target
                         if (hit.collider.CompareTag("Enemy"))
                         {
-                            Instantiate(hitEnemy, hit.point, Quaternion.LookRotation(hit.normal),hit.transform);
+                            Instantiate(weaponData[weaponIdChoice].hitEnemy, hit.point, Quaternion.LookRotation(hit.normal),hit.transform);
                             HitDamage?.Invoke(this, hit.transform);
                         }
                         else
                         {
-                            Instantiate(hitEnvironment, hit.point, Quaternion.LookRotation(hit.normal));
+                            Instantiate(weaponData[weaponIdChoice].hitEnvironment, hit.point, Quaternion.LookRotation(hit.normal));
                         }
                     }
                 }
                 break;
         }
-        
-        muzzleFlash.Play();
+
+        weaponData[weaponIdChoice].muzzleFlash.Play();
         currentAmmo--;
-        shootTimer = fireRate;
+        shootTimer = weaponData[weaponIdChoice].fireRate;
     }
     public float GetDamage()
     {
-        return damage;
+        return weaponData[weaponIdChoice].damage;
     }
     protected IEnumerator Reload(float timer)
     {
-        audioSource.clip = reload;
+        audioSource.clip = weaponData[weaponIdChoice].reload;
+        isChangingWeaponMode = true;
         canShoot = false;
         yield return new WaitForSeconds(timer);
-        currentAmmo = ammo;
+        currentAmmo = weaponData[weaponIdChoice].ammo;
         canShoot = true;
+        isChangingWeaponMode = false;
         isReloading = false;
         yield return null;
     }
     public int GetMaxAmmo()
     {
-        return ammo;
+        return weaponData[weaponIdChoice].ammo;
     }
     public int GetCurrentAmmo()
     {
         return currentAmmo;
+    }
+
+    protected void ChangeWeaponMode()
+    {
+        //horrible, but works for now
+        if(Input.GetKeyDown(KeyCode.Mouse1) && weaponIdChoice == 0 && !isChangingWeaponMode)
+        {
+            weaponIdChoice = 1;
+            StartCoroutine(Reload(1));
+        }
+        else if (Input.GetKeyDown(KeyCode.Mouse1) && weaponIdChoice == 1 && !isChangingWeaponMode)
+        {
+            weaponIdChoice = 0;
+            StartCoroutine(Reload(1));
+        }
     }
 }
